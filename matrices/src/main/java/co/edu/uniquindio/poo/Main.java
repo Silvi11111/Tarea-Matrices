cdb1c4426ea8fd52ec991fee56a7e8c67a7726b2
Sunny <silvanalopezgallego1@gmail.com>
2026-09-08 19:42:31 -0500

    .

---
+++ matrices/src/main/java/co/edu/uniquindio/poo/Main.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio.poo;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int [][] numeros={{2,5,6},{3,2,4},{1,9,8}};
+        //1. Imprimir matriz
+
+    }
+    public static int imprimirMatriz(int [][] matriz){
+        for (int i=0;i<matriz.length;i++){
+
+            for (int j=0;j<matriz[i].length;j++){
+
+            }
+        }
+    }
+}
